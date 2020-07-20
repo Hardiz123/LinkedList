98dd052249962278fadb7ffdb84de7733dbaf4d6
Hardiz123 <hardiknandwani420@gmail.com>
2020-07-21 01:38:58 +0530

    first commit

---
+++ LinkedList.java
@@ -0,0 +1,137 @@
+public class LinkedList {
+    private int size;
+    private Node start;
+
+    public  LinkedList() {
+        size =0;
+        start = null;
+    }
+    public void insertAtFirst(int val) {
+        Node n = new Node();
+        n.setData(val);
+        n.setNext(start);
+        start = n;
+        size++;
+    }
+    public void insertAtLast(int val) {
+        Node n,t;
+        n = new Node();
+        n.setData(val);
+        t = start;
+        if(t==null){
+            start=n;
+        }else{
+            while(t.getNext()!=null)
+            {
+                t=t.getNext();
+            }
+            t.setNext(n);
+        }
+        size++;
+    }
+
+    public void insertAtpos(int val , int pos) {
+        if(pos==1){
+            insertAtFirst(val);
+        }else if(pos==size+1){
+            insertAtLast(val);
+        }else if(pos>1 && pos<=size){
+            Node n = new Node(val,null);
+            Node t;
+            t= start;
+            for(int i=1;i<=pos-1;i++){
+                n.setNext(t.getNext());
+                t.setNext(n);
+                size++;
+            }
+        }else{
+            System.out.println("Insertion not possible");
+        }
+    }
+    public void insertInSortedList(int val){
+        Node n = new Node(val,null); //we create a new node that we want to add
+        Node current = start; // make a node and assign start value to is
+        Node temp = null; // make a temp node
+        while (current!=null && current.getData()<n.getData()) { // this is the condition
+            temp = current; //then current value will be in temp and current will getNext value
+            current = current.getNext();
+        }
+        n.setNext(current);// now we point our new node to the next value
+        temp.setNext(n); // we point our previous node to the new node that we created
+    }
+    
+    public void deleteFirst() {
+        if(start==null){
+            System.out.println("List is Empty cannot delete");
+        }else{
+            start = start.getNext();
+            size--;
+        }
+    }
+
+    public void deleteLast(){
+        if(start==null){
+            System.out.println("List is Empty cannot delete");
+        }else if(size==1)
+        {
+            start = null;
+            size--;
+        }else{
+            Node t;
+            t= start;
+            for(int i=1;i<=size-1;i++){
+                t =t.getNext();
+            }
+            t.setNext(null);
+            size--;
+        }
+    }
+
+    public void deleteAtPosition(int pos){
+        if(start==null)
+        System.out.println("list is already empty");
+        else if(pos<1 || pos>size)
+        {
+            System.out.println("Invalid position");
+        }else if(pos==1){
+            deleteFirst();
+        }else if(pos == size)
+        {
+            deleteLast();
+        }else{
+            Node t,t1;
+            t =start;
+            for(int i=1;i<pos-1;i++)
+            {
+                t=t.getNext();
+            }
+            t1 = t.getNext();
+            t.setNext(t1.getNext());
+        }
+    }
+    public boolean isEmpty(){
+
+        return(start == null);
+    }
+
+    public int  getListSize() {
+        return size;
+    }
+    public void viewList() {
+        Node t;
+        if(isEmpty()){
+            System.out.println("List is empty");
+        }
+        else{
+            t = start;
+            // for(int i=1;i<=size;i++){
+            //     System.out.print(t.getData()+ " ");
+            //     t = t.getNext();
+            // }
+            while (t!= null) {
+                System.out.println(t.getData());
+                t = t.getNext();
+            }
+        }
+    }
+}
---
+++ Test.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+/**
+ * Test
+ */
+
+public class Test {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        LinkedList list = new LinkedList();
+        boolean flag = true;
+        while (flag) {
+        System.out.println();
+        System.out.println("1.Add item to the list at start");
+        System.out.println("2.Add item to the last at the list");
+        System.out.println("3.Add Item tothe list at a position");
+        System.out.println("4. Delete first item from the list");
+        System.out.println("5. Delete last item from the list");
+        System.out.println("6. Delete item from the list with given position");
+        System.out.println("7. Insert in a sorted List");
+        System.out.println("8. view List");
+        System.out.println("9. exit");
+        System.out.println("Enter your choice");
+        int choice = sc.nextInt();
+        int position,value;
+
+        switch (choice) {
+            case 1:
+            System.out.println("Enter value of item");
+            value = sc.nextInt();
+            list.insertAtFirst(value);
+                
+                break;
+            case 2:
+            System.out.println("Enter value of item");
+            value = sc.nextInt();
+            list.insertAtLast(value);
+                break;       
+            case 3:
+            System.out.println("Enter position");
+            position = sc.nextInt();
+            System.out.println("Enter value of item");
+            value = sc.nextInt();
+            list.insertAtpos(value,position);
+                break;
+            case 4:
+            list.deleteFirst();
+                break;   
+            case 5:
+            list.deleteLast();
+                
+                break;
+            case 6:
+                System.out.println("Enter position");
+                position = sc.nextInt();
+                list.deleteAtPosition(position);
+                break;
+            case 7:
+                System.out.println("Enter value");
+                value = sc.nextInt();
+                list.insertInSortedList(value);
+                break;          
+            case 8:
+                list.viewList();
+                break;
+            case 9:
+                flag = false;  
+                break;
+            default:
+                System.out.println("Invalid Choice"); 
+                break;    
+        }
+        }
+
+        sc.close();
+    }
+}
